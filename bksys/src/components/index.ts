10733dafd8cba9c7152499cb3713e2ba1fe088fd
“zhenryx” <2273421799@qq.com/>
2024-08-19 16:17:07 +0800

    delete some files

--- bksys/src/components/index.ts
+++
@@ -1,9 +0,0 @@
-import SvgIcon from './SvgIcon/index.vue'
-const allGlobalComponent: any = { SvgIcon }
-export default {
-  install(app: any) {
-    Object.keys(allGlobalComponent).forEach((key: string) => {
-      app.component(key, allGlobalComponent[key])
-    })
-  }
-}
